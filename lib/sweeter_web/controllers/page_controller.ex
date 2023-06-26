30e8edb91854da04d252dd30972a9d01592d0542
an0h <an0h@decisived.com>
2023-06-25 20:18:18 -0700

    homepage showing imported list

--- lib/sweeter_web/controllers/page_controller.ex
+++ lib/sweeter_web/controllers/page_controller.ex
@@ -1,9 +1,13 @@
 defmodule SweeterWeb.PageController do
   use SweeterWeb, :controller
 
+  alias Sweeter.Repo
+  alias Sweeter.Content.Item
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    items = Item.get_all() |> Repo.preload(:images)
+    render(conn, :home, layout: false, items: items)
   end
 end
--- lib/sweeter_web/controllers/page_html.ex
+++ lib/sweeter_web/controllers/page_html.ex
@@ -2,4 +2,6 @@ defmodule SweeterWeb.PageHTML do
   use SweeterWeb, :html
 
   embed_templates "page_html/*"
+  embed_templates "item_html/*"
+  embed_templates "file_upload_html/*"
 end
