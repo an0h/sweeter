939460fcfbf139d7a8abef42f792434b7fef21d8
an0h <an0h@decisived.com>
2023-08-01 21:34:19 -0700

    adding all4u.city

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,7 +48,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "cherry.internetstate.city"
+  host = System.get_env("PHX_HOST") || "all4u.city"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :sweeter, SweeterWeb.Endpoint,
--- lib/sweeter_web/mails/pow_mailer.ex
+++ lib/sweeter_web/mails/pow_mailer.ex
@@ -8,7 +8,7 @@ defmodule SweeterWeb.Pow.Mailer do
   def cast(%{user: user, subject: subject, text: text, html: html}) do
     new_email(
       to: user.email,
-      from: "no-reply@cherry.internetstate.city",
+      from: "no-reply@all4u.city",
       subject: subject,
       html_body: html,
       text_body: text
