647ea400d38f98ff4499c1752e3094baffff6dc9
an0h <an0h@decisived.com>
2023-12-21 12:36:56 -0800

    modal background color

--- lib/sweeter_web/components/core_components.ex
+++ lib/sweeter_web/components/core_components.ex
@@ -66,7 +66,7 @@ defmodule SweeterWeb.CoreComponents do
               phx-window-keydown={JS.exec("data-cancel", to: "##{@id}")}
               phx-key="escape"
               phx-click-away={JS.exec("data-cancel", to: "##{@id}")}
-              class="shadow-slate-500/10 ring-slate-500/10 relative hidden rounded-2xl bg-white p-14 shadow-lg ring-1 transition"
+              class="shadow-gray-slate-500/10 ring-slate-500/10 relative hidden rounded-2xl bg-gray-800 p-14 shadow-lg ring-1 transition"
             >
               <div class="absolute top-6 right-5">
                 <button
