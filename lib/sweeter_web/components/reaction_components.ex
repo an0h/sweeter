2b7715f015b741c1850d3a935182ecfbbe3e13e7
an0h <an0h@decisived.com>
2023-12-22 17:56:18 -0800

    reaction is a component

--- lib/sweeter_web/components/reaction_components.ex
+++ lib/sweeter_web/components/reaction_components.ex
@@ -6,9 +6,19 @@ defmodule SweeterWeb.ReactionComponents do
 
   # Include the LiveComponent in your heex template
   def reactioncomponent(assigns) do
-    ~L"""
-    <div>
-    the render of reactioncomponents
+    ~H"""
+    <div id="your-reaction"></div>
+    <div class="reactions">
+    <ul>
+    <%= for reaction <- @item.reactions do %>
+    <li><%= reaction.emoji %><span class="reaction-count"><%= reaction.count %></span></li>
+    <% end %>
+    </ul>
+    </div>
+    <div class="add-reactions">
+    <div id="emoji-trigger">your reactions</div>
+    <input type="hidden" id="item_id" value={@item.id}>
+    <input type="hidden" id="address" value={@address}>
     </div>
     """
   end
