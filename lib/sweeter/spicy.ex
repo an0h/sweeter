a4e82c748273e4170dfee56ca1c56ccec1bead06
an0h <an0h@decisived.com>
2024-04-24 19:12:11 -0700

    buildingurlSendToken added

--- lib/sweeter/spicy.ex
+++ lib/sweeter/spicy.ex
@@ -6,7 +6,7 @@ defmodule Sweeter.Spicy do
 
   def get_new_user_address(email, mnemonic) do
     IO.puts "in get new user address"
-    url = buildurl(email, mnemonic)
+    url = buildurlRegister(email, mnemonic)
 
     IO.puts url
     IO.puts "after url"
@@ -70,6 +70,30 @@ defmodule Sweeter.Spicy do
       {"Content-Type", "application/json"}
     ]
 
+    url = buildurlSendToken(address, "cosmos1ut70cd0krgtr4pxjvz5jyc2jnh7788gyl8p7mc", value)
+
+    IO.puts url
+
+    response = HTTPoison.get!(url, headers)
+
+    # Access the response status code, headers, and body
+    # status_code = response.status_code
+    # response_headers = response.headers
+    # response_body =
+    IO.puts "before response"
+    IO.inspect response
+    IO.puts "after response"
+    Poison.decode!(response.body)
+  end
+
+  def add_spicy_token_faucet(address, value) do
+
+    IO.puts "in add token faucet"
+    headers = [
+      {"accept", "application/json"},
+      {"Content-Type", "application/json"}
+    ]
+
     body = %{
       "address" => address,
       "coins" => [value]
@@ -117,13 +141,18 @@ defmodule Sweeter.Spicy do
     Poison.decode!(response.body)
   end
 
-  defp buildurl(email, mnemonic) do
+  defp buildurlRegister(email, mnemonic) do
     spicy_service = fetchSpicy5555()
     if mnemonic == nil or mnemonic == "" do
       URI.encode("#{spicy_service}/?name=#{email}")
     else
       URI.encode("#{spicy_service}/?name=#{email}&mnemonic=#{mnemonic}")
     end
+  end
+
+  defp buildurlSendToken(to_account, from_account, amount) do
+    spicy_service = fetchSpicy5555()
+    URI.encode("#{spicy_service}/send_tokens?to_account=#{to_account}&from_account=#{from_account}&amount=#{amount}")
   end
 
   defp fetchSpicy1317() do
